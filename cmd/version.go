8511651553f6a7b3ebc56dd7a9edd1f07d1bf9af
odino <alessandro.nadalin@gmail.com>
2018-02-13 00:43:25 +0400

    version command

---
+++ cmd/version.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of docsql",
+	Long:  `All software has versions. This is docsql's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("docsql v1.0.0")
+	},
+}
