bdfd6deeaf5dfb34ee1e00f23c48e0f1658c6d17
odino <alessandro.nadalin@gmail.com>
2018-05-18 17:16:35 +0400

    bumped version

--- cmd/version.go
+++ cmd/version.go
@@ -15,6 +15,6 @@ var versionCmd = &cobra.Command{
 	Short: "Print the version number of docsql",
 	Long:  `All software has versions. This is docsql's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("docsql v1.0.0")
+		fmt.Println("docsql v1.0.1")
 	},
 }
